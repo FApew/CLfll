afb982ea21a9b6d10780ce39903e966ea17ba07b
fffppp-pixel <francype06@gmail.com>
2024-04-13 21:02:36 +0200

    base del main finita

---
+++ src/StaticMain/JavaScript/ScriptStatic.js
@@ -0,0 +1,12 @@
+const masteryear = document.getElementById("masterYear")
+const poweryear = document.getElementById("powerYear")
+
+
+
+masteryear.addEventListener("mousedown", () => {
+    window.open("#")
+})
+
+poweryear.addEventListener("mousedown", () => {
+    window.open("#")
+})
